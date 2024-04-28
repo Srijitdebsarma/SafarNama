0f4d33f923c27ce1e05beb87a121a09487e67abf
srijitDebsarma <srijitdebsarma@gmail.com>
2024-04-28 13:22:09 +0530

    Ready

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express=require("express");
-const wrapAsync = require("../utils/wrapAsync");
+const wrapAsync = require("../utils/wrapAsync.js");
 const router = express.Router();
 const User=require("../models/user.js");
 const passport=require("passport");
